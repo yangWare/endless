c3cfe674fceb5722a0a8536389a962b30474e056
XDao <495903417@qq.com>
2025-04-05 17:14:04 +0800

    优化路由实现

--- endless_v2/vite.config.js
+++ endless_v2/vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/endless/',
+  base: '/endless/static/',
   plugins: [vue()],
   build: {
     assetsDir: 'assets',
